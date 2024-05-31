1b2f9d61bdd14fe01489dfbf845daaee0ac2a9a8
Nik Graf <nik@nikgraf.com>
2024-05-31 10:41:06 +0200

    setup monorepo

--- apps/koriko/metro.config.js
+++ apps/koriko/metro.config.js
@@ -14,7 +14,13 @@ config.resolver.unstable_enablePackageExports = true;
 
 // Needed for monorepo setup (can be removed in standalone projects)
 const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, "../..");
 
-config.resolver.nodeModulesPaths = [path.resolve(projectRoot, "node_modules")];
+config.watchFolders = [monorepoRoot];
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(monorepoRoot, "node_modules"),
+];
 
 module.exports = withNativeWind(config, { input: "./src/global.css" });
