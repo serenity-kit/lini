701868f7ea760099f9dfad1427fa40c19071d184
Nik Graf <nik@nikgraf.com>
2024-05-31 10:02:18 +0200

    fix tailwind setup

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,5 @@ module.exports = (api) => {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: ["babel-plugin-transform-vite-meta-env"],
   };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const { hairlineWidth } = require("nativewind/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
